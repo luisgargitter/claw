ff15bdc39cf222403f01931dbc83cbd156f728ac
Luis Gargitter <luis.gargitter@gmx.net>
2023-01-21 14:41:03 +0100

    translated bash script to python and added some functionality. For further developement see TODO's in convert.py.

---
+++ src/convert.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import re
+
+def extract_paragraphs(articles):
+    for i, article in enumerate(articles):
+        articles[i] = re.sub("\(ex-Artikel \d+ .+", " ", article).strip()
+
+    # TODO:
+    #   - remove footnotes.
+    #   - split paragraphs.
+    # Note: maybe it is easier to first split by paragraphs and the remove the footnotes,
+    # because they not match the internal numbering order of the paragraph (inspect document in legaldocs/.
+
+    return articles
+
+def split_articles(text):
+    return re.split("\nArtikel \d+\n", text) 
+
+def main(args):
+    with open(args[1], "r") as f:
+        text = f.read()
+    articles = split_articles(text)[1:]
+    articles = extract_paragraphs(articles)
+
+    for a in articles:
+        print(a)
+        print("------------")
+
+if __name__ == "__main__":
+    main(sys.argv)
