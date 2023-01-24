259ac07e3031a4744a00b5d4ada4fcb94e328b75
Luis Gargitter <luis.gargitter@gmx.net>
2023-01-24 01:58:58 +0100

    minor changes to convert.py, tried to implement new functionality(failed)

--- src/convert.py
+++ src/convert.py
@@ -2,9 +2,20 @@ import os
 import sys
 import re
 
+// die header stimmen noch ned
+header1 = "26.10.2012\n\nDE\n\n.\n\nC 326\w+\n\n"
+header2 = "C 326\w+\n\nDE\n\n.\n\n26.10.2012\n\n" 
+
+def strip_to_last_sentence(article):
+    article += "\n"
+    ind = article.rfind(".\n")
+    if(ind <= 0):
+        ind = len(article)-2
+    return article[:ind+2]
+
 def extract_paragraphs(articles):
     for i, article in enumerate(articles):
-        articles[i] = re.sub("\(ex-Artikel \d+ .+", " ", article).strip()
+        articles[i]= strip_to_last_sentence(article).strip()
 
     # TODO:
     #   - remove footnotes.
@@ -15,12 +26,19 @@ def extract_paragraphs(articles):
     return articles
 
 def split_articles(text):
-    return re.split("\nArtikel \d+\n", text) 
+    return re.split("\nArtikel\s+\d+\n", text) 
+
+def remove_unwanted(text):
+    res = re.sub("\(ex-Artikel\s+\d+\s.+", "", text)
+    res = re.sub(header1, "", res)
+    res = re.sub(header2, "", res)    
+
+    return res
 
 def main(args):
     with open(args[1], "r") as f:
         text = f.read()
-    articles = split_articles(text)[1:]
+    articles = split_articles(remove_unwanted(text))[1:]
     articles = extract_paragraphs(articles)
 
     for a in articles:
